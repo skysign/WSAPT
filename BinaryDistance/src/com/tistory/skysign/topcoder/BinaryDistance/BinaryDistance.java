3c7562101495b69f602ed9731da422cd45bdf86d
ByeongKeon Kin <skysign@gmail.com>
2019-11-26 16:13:39 +0900

    Update BinaryDistance in topcoder

--- BinaryDistance/src/com/tistory/skysign/topcoder/BinaryDistance/BinaryDistance.java
+++ BinaryDistance/src/com/tistory/skysign/topcoder/BinaryDistance/BinaryDistance.java
@@ -1,64 +1,111 @@
 package com.tistory.skysign.topcoder.BinaryDistance;
 
 public class BinaryDistance {
-    public int findMyLevel(int V, int r) {
-        if(V == 1){
-            return r;
+//    첫번째 풀기 시도한 것
+//    풀기는 했지만, 코드가 너무 길고, 풀이가 복잡함
+//    public int findMyLevel(int V, int r) {
+//        if(V == 1){
+//            return r;
+//        }
+//
+//        return findMyLevel(V/2, r+1);
+//    }
+//
+////    1 : left
+////    0 : right
+//    public int findLR(int V) {
+//        if(V > 3) {
+//            return findLR(V/2);
+//        }
+//
+//        return (V == 2)? 1: 0;
+//    }
+//
+//    public int maxDist(int N, int V) {
+////      리턴값
+//        int r = 0;
+//
+//        // 트리의 높이 l
+//        int l = 0;
+//
+//        // level층 까지의 숫자 합
+//        int levelSum = 0;
+//
+//        // V 가 트리의 왼편인지 오른편인지
+//        int LR = 0;
+//
+//        boolean isRightShort = false;
+//        for(int i=0; true; ++i) {
+//            levelSum += Math.pow(2,i);
+//
+//            if( (levelSum < N) && (N <= levelSum+Math.pow(2,i+1)) ) {
+//                if(N <= levelSum + (Math.pow(2,i+1)/2)) {
+//                    isRightShort = true;
+//                }
+//                else {
+//                    isRightShort = false;
+//                }
+//
+//                l = i+1;
+//                break;
+//            }
+//        }
+//
+//        int ll = findMyLevel(V, 0);
+//        if(isRightShort) {
+//            LR = findLR(V);
+//            r = ll + ((l) - LR);
+//        }
+//        else {
+//            r = ll + (l);
+//        }
+////        System.out.printf("levels : %d\n", l);
+//        return r;
+//    }
+
+    // SRM 768 EDITORIALS를 보고 난뒤,
+    // 보다 빠른 방법을 풀어봄
+    // is_left()와 depth()를 하나로 합처서, depth_LR()로 계산함
+    // left : LR[0](true)
+    // right : LR[0](false)
+    public int depth_LR(int a, boolean[] LR) {
+        int v = a;
+        int d = 1;
+        while(v>3) {
+            d++;
+            v >>= 1;
         }
 
-        return findMyLevel(V/2, r+1);
-    }
-
-//    1 : left
-//    0 : right
-    public int findLR(int V) {
-        if(V > 3) {
-            return findLR(V/2);
+        if(v == 2) {
+            LR[0] = true;
+        }
+        else {
+            LR[0] = false;
         }
 
-        return (V == 2)? 1: 0;
+        return d;
     }
 
     public int maxDist(int N, int V) {
-//      리턴값
-        int r = 0;
+        int dN, dV;
+        boolean[] lrN = {true};
+        boolean[] lrV = {true};
 
-        // 트리의 높이 l
-        int l = 0;
+        int r;
 
-        // level층 까지의 숫자 합
-        int levelSum = 0;
-
-        // V 가 트리의 왼편인지 오른편인지
-        int LR = 0;
-
-        boolean isRightShort = false;
-        for(int i=0; true; ++i) {
-            levelSum += Math.pow(2,i);
-
-            if( (levelSum < N) && (N <= levelSum+Math.pow(2,i+1)) ) {
-                if(N <= levelSum + (Math.pow(2,i+1)/2)) {
-                    isRightShort = true;
-                }
-                else {
-                    isRightShort = false;
-                }
-
-                l = i+1;
-                break;
-            }
+        if(V == 1) {
+            return depth_LR(N,lrN);
         }
 
-        int ll = findMyLevel(V, 0);
-        if(isRightShort) {
-            LR = findLR(V);
-            r = ll + ((l) - LR);
-        }
-        else {
-            r = ll + (l);
-        }
+        dN = depth_LR(N, lrN);
+        dV = depth_LR(V, lrV);
 
-//        System.out.printf("levels : %d\n", l);
+        r = dN + dV;
+
+        if(lrN[0]) {
+            if(lrV[0])
+                r--;
+        }
 
         return r;
     }
