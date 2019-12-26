dbb6079605204cf3c32a13e4f6ebdef348dc7250
ByeongKeon Kin <skysign@gmail.com>
2019-12-26 17:54:39 +0900

    Update A - Frog 1 / atcoder.jp
    문제 링크 : https://atcoder.jp/contests/dp/tasks/dp_a
    Reader() 를 사용해서 속도 빠르게 함
    다른 로직은 똑같은대, Scanner 안쓰고, Reader사용했더니
    4xxms 대에서 104ms 로 빨라졌음 젠장... 쩝... s(>_<)a

--- atcoder.jp/A - Frog 1/src/Main.java
+++ atcoder.jp/A - Frog 1/src/Main.java
@@ -1,11 +1,141 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 // A - Frog 1 / atcoder.jp
 // 문제 링크 : https://atcoder.jp/contests/dp/tasks/dp_a
 
 public class Main {
-    public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
+    // 이런 방법이 있을 줄이야.. - -;
+    // 어쩐지 다른 애들이 한거 보면, 나보다 한 300ms 빠르던대,
+    // Scanner를 사용하지 않고, 따로 Reader를 만들어 써서 그렇구나... 젠장...
+    // https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader sc = new Reader();
 	    int N = sc.nextInt();
 	    int[] stones = new int[N];
 
