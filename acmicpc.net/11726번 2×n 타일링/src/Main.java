10046ccac595ee6c8be57b88e3e917f5a7ec5308
ByeongKeon Kin <skysign@gmail.com>
2020-01-23 22:29:40 +0900

    11726번 2×n 타일링 / BAEKJOON ONLINE JUDGE / acmicpc.net 문제링크 : https://www.acmicpc.net/problem/11726 제출링크 : https://www.acmicpc.net/source/17183178

---
+++ acmicpc.net/11726번 2×n 타일링/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 11726번 2×n 타일링 / BAEKJOON ONLINE JUDGE / acmicpc.net
+ * 문제링크 : https://www.acmicpc.net/problem/11726
+ * 제출링크 : https://www.acmicpc.net/source/17183178
+ */
+public class Main {
+    public void solve() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] d = new int[N+1];
+
+        d[1] = 1;
+
+        if(N>=2)
+            d[2] = 2;
+
+        for(int i=3; i<=N; ++i) {
+            d[i] = (d[i-1] + d[i-2]) % 10007;
+        }
+
+        System.out.println(d[N]);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solve();
+    }
+}
