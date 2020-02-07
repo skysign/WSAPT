e09ed48c30b4d27f20fa8d6abf0d3843f6011fc8
ByeongKeon Kin <skysign@gmail.com>
2020-02-07 11:17:59 +0900

    Update TRIANGLEPATH 삼각형 위의 최대 경로 / ALGOSPOT
    문제링크 : https://algospot.com/judge/problem/read/TRIANGLEPATH
    제출링크 : https://algospot.com/judge/submission/detail/655087
    
    maxSum() 이중 루프를 이용한 DP 풀이
    maxSum_rec() recursion과 dP를 이용한 풀이

--- ALGOSPOT/TRIANGLEPATH/src/Main.java
+++ ALGOSPOT/TRIANGLEPATH/src/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
  * TRIANGLEPATH 삼각형 위의 최대 경로 / ALGOSPOT
  * 문제링크 : https://algospot.com/judge/problem/read/TRIANGLEPATH
  * 제출링크 : https://algospot.com/judge/submission/detail/655087
+ *
+ * maxSum() 이중 루프를 이용한 DP 풀이
+ * maxSum_rec() recursion과 dP를 이용한 풀이
  */
 public class Main {
     Scanner sc = new Scanner(System.in);
@@ -23,6 +26,10 @@ public class Main {
 
             r = maxSum(map, N);
             System.out.println(r);
+
+            int[][] dp = new int[N+1][N+1];
+            r = maxSum_rec(map, 1, 1, N, dp);
+            System.out.println(r);
         }
     }
 
@@ -40,6 +47,25 @@ public class Main {
             max = Math.max(max, dp[N][j]);
 
         return max;
+    }
+
+    public int maxSum_rec(int[][] map, int idxI, int idxJ, int N, int[][] dp) {
+        int r = 0;
+        int i = idxI;
+        int j = idxJ;
+
+        if(i == N) {
+            dp[i][j] = map[i][j];
+            return dp[i][j];
+        }
+
+        if(dp[i][j] != 0)
+            return dp[i][j];
+
+        dp[i][j] = Math.max(maxSum_rec(map, idxI+1, idxJ, N, dp),
+                maxSum_rec(map, idxI+1, idxJ+1, N, dp)) + map[i][j];
+
+        return dp[i][j];
     }
 
     public static void main(String[] args) {
