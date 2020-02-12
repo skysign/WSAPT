0a767b51380f1c43741041336ac6a36f764bf0d0
ByeongKeon Kin <skysign@gmail.com>
2020-02-12 21:19:40 +0900

    51. N-Queens / LeetCode
    문제링크 : https://leetcode.com/problems/n-queens/
    제출링크 : https://leetcode.com/problems/n-queens/submissions/

---
+++ LeetCode/51. N-Queens/src/Main.java
@@ -0,0 +1,464 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 51. N-Queens / LeetCode
+ * 문제링크 : https://leetcode.com/problems/n-queens/
+ * 제출링크 : https://leetcode.com/problems/n-queens/submissions/
+ */
+public class Main {
+    int N;
+    int[][] map;
+    List<List<String>> LL;
+    ArrayList<Integer> alI;
+    ArrayList<Integer> alJ;
+
+    public void solve() throws IOException {
+        N = sc.nextInt();
+
+        List<List<String>> r = solveNQueens(N);
+        for(List<String> ls: r) {
+            for(String S: ls) {
+                println(S);
+            }
+            println();
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        N = n;
+        map = new int[n][n];
+        LL = new ArrayList<>();
+        alI = new ArrayList<>();
+        alJ = new ArrayList<>();
+
+        fill2D(map, -1);
+        sq(0, 0,0, n);
+
+        return LL;
+    }
+
+    public void sq(int begI, int begJ, int idx, int N) {
+        for(int i=begI; i<N; ++i) {
+            for(int j=begJ; j<N; ++j) {
+                if(CanPut(i, j)) {
+                    mark(i, j, idx);
+                    if(idx+1 == N) {
+                        gen();
+                    }
+                    else {
+                        int nextI = i+1;
+                        sq(nextI, 0, idx + 1, N);
+                    }
+                    unmark(i, j, idx);
+                }
+            }
+        }
+    }
+
+    public void gen() {
+        ArrayList<String> rtn = new ArrayList();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; ++i) {
+            sb.append(".");
+        }
+
+        for(int i=0; i<N; ++i) {
+            rtn.add(sb.toString());
+        }
+
+        for(int k = 0; k<alI.size(); ++k) {
+            int i = alI.get(k);
+            int j = alJ.get(k);
+            char[] s = rtn.get(i).toCharArray();
+            s[j] = 'Q';
+            rtn.set(i, new String(s));
+        }
+
+        LL.add(rtn);
+    }
+
+    boolean CanPut(int i, int j) {
+        if(map[i][j] == -1)
+            return true;
+
+        return false;
+    }
+
+    void mark(int i, int j, int idx) {
+        alI.add(i);
+        alJ.add(j);
+        _domark(i, j, -1, idx);
+    }
+
+    void unmark(int i, int j, int idx) {
+        _domark(i, j, idx, -1);
+        alI.remove(alI.size()-1);
+        alJ.remove(alJ.size()-1);
+    }
+
+    void _domark(int i, int j, int ori, int idx) {
+        for(int k=0; k<d8i.length; ++k) {
+            int di = d8i[k];
+            int dj = d8j[k];
+
+            int markI = i;
+            int markJ = j;
+
+            while(0<= markI && markI < N &&
+                    0<= markJ && markJ < N) {
+                if(map[markI][markJ] == ori)
+                    map[markI][markJ] = idx;
+
+                markI += di;
+                markJ += dj;
+            }
+        }
+    }
+
+    public void _solve() throws IOException {
+        solve();
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main._solve();
+    }
+
+//        int n = ;
+//        for(int i=0; i<n; ++i) {
+//
+//        }
+
+//        int n = ;
+//        for(int j=0; j<n; ++j) {
+//
+//        }
+
+//        int n = ;
+//        for(int i=0; i<n; ++i) {
+//            for(int j=0; j<n; ++j) {
+//
+//            }
+//        }
+
+//        int N = ;
+//        for(int i=0; i<n; ++i) {
+//            for(int j=0; j<n; ++j) {
+//                for(int k=0; k<n; ++k) {
+//
+//                }
+//            }
+//        }
+
+    // Normally used MIN/MAX number
+    public final int _1087654321 = 1087654321;
+    public final int __1087654321 = -1087654321;
+    public final int _987654321 = 987654321;
+    public final int __987654321 = -987654321;
+
+    // Commonly used numbers as a divider of mod operation to prevent overflow
+    public final int _10_007 = 10007;
+    public final int _1_000_000_007 = 1000000007;
+    final int MOD = _1_000_000_007;
+
+    public void print(String s) throws IOException {
+        bw.write(s);
+    }
+
+    public void println() throws IOException {
+        print("\n");
+    }
+
+    public void println(int a) throws IOException {
+        print(a+"\n");
+    }
+
+    public void println(long a) throws IOException {
+        print(a+"\n");
+    }
+
+    public void println(double a) throws IOException {
+        print(a+"\n");
+    }
+
+    public void println(String s) throws IOException {
+        bw.write(s+"\n");
+    }
+
+    // -1
+    public final int MINUS_1 = -1;
+
+    // 4 ways
+    public int[] d4i = new int[]{-1, 1, 0, 0};
+    public int[] d4j = new int[]{0, 0, -1, 1};
+
+    // 8 ways
+    public int[] d8i = new int[]{-1, 1, 0, 0,  -1, -1, 1, 1};
+    public int[] d8j = new int[]{0, 0, -1, 1,  -1, 1, 1, -1};
+
+    // Initialize 2D arrays with value v
+    public void fill2D(int[][] _2D, int v) {
+        for(int[] _1D: _2D) {
+            Arrays.fill(_1D, v);
+        }
+    }
+
+    public void fill2D(long[][] _2D, long v) {
+        for(long[] _1D: _2D) {
+            Arrays.fill(_1D, v);
+        }
+    }
+
+    public void fill2D(double[][] _2D, double v) {
+        for(double[] _1D: _2D) {
+            Arrays.fill(_1D, v);
+        }
+    }
+
+    public void print2D(int[][] dp) throws IOException {
+        print("    ");
+
+        for(int j=0; j<dp[0].length; ++j){
+            print(SS(j)+j + " ");
+        }
+        println();
+
+        for(int i=0; i<dp.length; ++i){
+            print(SS(i)+i+"|");
+            for(int j=0; j<dp[0].length; ++j){
+                print(SS(dp[i][j])+dp[i][j] + " ");
+            }
+            println();
+        }
+    }
+
+    public void print2D(long[][] dp) throws IOException {
+        print("    ");
+
+        for(int j=0; j<dp[0].length; ++j){
+            print(SS(j)+j + " ");
+        }
+        println();
+
+        for(int i=0; i<dp.length; ++i){
+            print(SS(i)+i+"|");
+            for(int j=0; j<dp[0].length; ++j){
+                print(SS(dp[i][j])+dp[i][j] + " ");
+            }
+            println();
+        }
+    }
+
+    public final int LENGHT_OF_NUMBER = 3;
+    /**
+     * Generate space string for alignment when we print 2D arrays.
+     * its length is LENGHT_OF_NUMBER - the length of number
+     * Basically, we assume that number a is less than 1000.
+     * So, LENGHT_OF_NUMBER is 3 by default.
+     * @param a
+     * @return
+     */
+    public String SS(int a) {
+        String s = a+"";
+        int l = LENGHT_OF_NUMBER - s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<l; ++i) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public String SS(long a) {
+        String s = a+"";
+        int l = LENGHT_OF_NUMBER - s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<l; ++i) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    // Initialize 3D arrays with value v
+    public void fill3D(int[][][] _3D, int v) {
+        for(int[][] _2D: _3D) {
+            for(int[] _1D: _2D) {
+                Arrays.fill(_1D, v);
+            }
+        }
+    }
+
+    // GCD
+    int GCD(int a, int b) {
+        if(0 == b) {
+            return a;
+        }
+
+        return GCD(b, a%b);
+    }
+
+    long GCD(long a, long b) {
+        if(0 == b) {
+            return a;
+        }
+
+        return GCD(b, a%b);
+    }
+
+    boolean IsEven(int a) {
+        return (a>>1) == ((a>>1)<<1);
+    }
+
+    boolean IsOdd(int a) {
+        return (a>>1) != ((a>>1)<<1);
+    }
+
+    boolean IsEven(long a) {
+        return (a>>1) == ((a>>1)<<1);
+    }
+
+    boolean IsOdd(long a) {
+        return (a>>1) != ((a>>1)<<1);
+    }
+
+    public Reader sc = new Reader();
+//    Sometimes, Reader class cause unknown problem, when I submit my java code to judge server.
+//    For more detail, please see https://algospot.com/forum/read/4731/
+//    public Scanner sc = new Scanner(System.in);
+
+    public BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    // https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+    public class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String nextLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+}
