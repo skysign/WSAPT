09ea900e58ab2023e0ac741ed677aab4ff0d6cf8
ByeongKeon Kin <skysign@gmail.com>
2020-03-21 18:13:07 +0900

    Update ProblemSolving_CPP_template
    nullptr is used

--- ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
+++ ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     std::cout << "Hello World!\n";
 }
