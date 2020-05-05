7a16d285998119ffb08d428359cc1057f795d90e
ByeongKeon Kim <skysign@gmail.com>
2020-05-05 12:43:36 +0900

    Update cpp template

--- ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
+++ ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
@@ -1,10 +1,28 @@
+/**
+ *  / BOJ
+ * 문제링크 : 
+ * 제출링크 : 
+ */
+
 #include <iostream>
 using namespace std;
+
+void solve();
+
+void solve() {
+	int N;
+	cin >> N;
+	
+	for(int i=0; i<N; ++i) {
+
+	}
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    std::cout << "Hello World!\n";
+
+    solve();
 }
