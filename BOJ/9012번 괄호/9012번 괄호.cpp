50f03581a3d4cd6cf57335d40734b7d269ec0364
ByeongKeon Kim <skysign@gmail.com>
2020-05-05 18:14:43 +0900

    BOJ 9012번 괄호
    문제링크 : https://www.acmicpc.net/problem/9012
    제출링크 : https://www.acmicpc.net/source/19609114

---
+++ BOJ/9012번 괄호/9012번 괄호.cpp
@@ -0,0 +1,55 @@
+﻿/**
+ * BOJ 9012번 괄호
+ * 문제링크 : https://www.acmicpc.net/problem/9012
+ * 제출링크 : https://www.acmicpc.net/source/19609114
+ */
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+void solve();
+
+void solve() {
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    for (int i = 0; i < N; ++i) {
+        string s;
+        getline(cin, s);
+        stack<char> st;
+        string r = "YES";
+
+        for (char c : s) {
+            if ('(' == c)
+                st.push(c);
+            if (')' == c) {
+                if ((st.size() > 0) && ('(' == st.top())) {
+                    st.pop();
+                }
+                else {
+                    r = "NO";
+                    break;
+                }
+            }
+        }
+
+        if (st.size() > 0) {
+            cout << "NO" << '\n';
+            continue;
+        }
+
+        cout << r << '\n';
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
