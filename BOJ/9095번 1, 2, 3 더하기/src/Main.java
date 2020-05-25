4e62ff24a59e18d9a4e656bbd6cb08ebbb06a930
ByeongKeon Kim <skysign@gmail.com>
2020-05-25 17:36:55 +0900

    BOJ 9095번 1, 2, 3 더하기
    문제링크 : https://www.acmicpc.net/problem/9095
    제출링크 : https://www.acmicpc.net/source/17177201
    문제풀이 : https://skysign.tistory.com/178

--- BOJ/9095번 1, 2, 3 더하기/src/Main.java
+++ BOJ/9095번 1, 2, 3 더하기/src/Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
 /**
- * 9095번 1, 2, 3 더하기 / BAEKJOON ONLINE JUDGE / acmicpc.net
+ * BOJ 9095번 1, 2, 3 더하기
  * 문제링크 : https://www.acmicpc.net/problem/9095
  * 제출링크 : https://www.acmicpc.net/source/17177201
+ * 문제풀이 : https://skysign.tistory.com/178
  */
 public class Main {
+    int[] dp;
+
     public void solve() {
         Scanner sc = new Scanner(System.in);
+        ans(10);
         int T = sc.nextInt();
 
         for(int t=0; t<T; ++t) {
             int N = sc.nextInt();
-            int r = ans(N);
-            System.out.println(r);
+            System.out.println(dp[N]);
         }
     }
 
-    public int ans(int N) {
+    public void ans(int N) {
         /**
          * d_1 = 1 : 1
          * d_2 = 1+1 : 2
@@ -28,22 +31,20 @@ public class Main {
          * 왠지 느낌에, d_i = d_i-1 + d_i-2 + d_i-1 인 것 같습니다. d_7 으로 확인해 보고, 44가 나오면
          * 이 점화식으로 풀어 보겠습니다.
          */
-        int[] dp = new int[N+1];
+        dp = new int[N+1];
 
-        if(N+1 >= 1+1)
+        if(N >= 1)
             dp[1] = 1;
 
-        if(N+1 >= 2+1)
+        if(N >= 2)
             dp[2] = 2;
 
-        if(N+1 >= 3+1)
+        if(N >= 3)
             dp[3] = 4;
 
         for(int n=4; n<=N; ++n) {
             dp[n] = dp[n-1] + dp[n-2] + dp[n-3];
         }
-
-        return dp[N];
     }
 
     public static void main(String[] args) {
