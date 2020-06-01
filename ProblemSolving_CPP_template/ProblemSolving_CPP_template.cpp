8c7cbcd0230cf6f462efedc582976a10d67b6691
ByeongKeon Kim <skysign@gmail.com>
2020-06-01 10:25:11 +0900

    Update cpp template

--- ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
+++ ProblemSolving_CPP_template/ProblemSolving_CPP_template.cpp
@@ -1,11 +1,24 @@
 /**
- *  / BOJ
+ * BOJ 
  * 문제링크 : 
  * 제출링크 : 
+ * 문제풀이 : 
+ * 유튜브 문제풀이 : 
+ * CPP소스 : 
+ * 자바소스 : 
  */
 
 #include <iostream>
+#include <algorithm>	// min() max()
+#include <vector>
+#include <stack>
 using namespace std;
+
+#define N_MAX (1000+1)
+
+int LCM(int a, int b);
+int GCD(int a, int b);
+bool IsPrimeNnumber(int n);
 
 void solve();
 
@@ -25,4 +38,30 @@ int main()
     cout.tie(nullptr);
 
     solve();
+}
+
+// LCM
+int LCM(int a, int b) {
+    return a * (b / GCD(a, b));
+}
+
+// GCD
+int GCD(int a, int b) {
+    if (0 == b) {
+        return a;
+    }
+
+    return GCD(b, a % b);
+}
+
+// prime number
+bool IsPrimeNnumber(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
 }
