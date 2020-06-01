862867b5ab63d4b88739f2b90b4223b3ff338cbd
ByeongKeon Kim <skysign@gmail.com>
2020-06-01 11:57:40 +0900

    BOJ 1699번 제곱수의 합
    문제링크 : https://www.acmicpc.net/problem/1699
    제출링크 : https://www.acmicpc.net/source/20128697
    문제풀이 : https://skysign.tistory.com/249

--- BOJ/1699번 제곱수의 합/1699번 제곱수의 합.cpp
+++ BOJ/1699번 제곱수의 합/1699번 제곱수의 합.cpp
@@ -1,11 +1,8 @@
-/**
- * BOJ 
- * 문제링크 : 
- * 제출링크 : 
- * 문제풀이 : 
- * 유튜브 문제풀이 : 
- * CPP소스 : 
- * 자바소스 : 
+﻿/**
+ * BOJ 1699번 제곱수의 합
+ * 문제링크 : https://www.acmicpc.net/problem/1699
+ * 제출링크 : https://www.acmicpc.net/source/20128697
+ * 문제풀이 : https://skysign.tistory.com/249
  */
 
 #include <iostream>
@@ -20,15 +17,25 @@ bool IsPrimeNnumber(int n);
 
 void solve();
 
-#define N_MAX (1000+1)
+#define N_MAX (100000+1)
+int dp[N_MAX] = {0, };
 
 void solve() {
-	int N;
-	cin >> N;
-	
-	for(int i=0; i<N; ++i) {
+    int N;
+    cin >> N;
 
-	}
+    for (int i = 1; i <= N; ++i) {
+        // 어떤 dp[i]가 가질 수 있는 최대 값은, N_MAX 입니다.
+        // 1^2 으로 N_MAX 번 더해서, N 이라는 숫자를 만든 것입니다.
+        dp[i] = N_MAX;
+        for (int j = 1; j*j <= i; ++j) {
+            if ((i - (j * j)) >= 0) {
+                dp[i] = min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+    }
+
+    cout << dp[N];
 }
 
 int main()
@@ -38,8 +45,8 @@ int main()
     cout.tie(nullptr);
 
     solve();
-	
-	return 0;
+
+    return 0;
 }
 
 // LCM
