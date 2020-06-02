44840cf78401da558016f047e5bfe41f7e171efd
ByeongKeon Kim <skysign@gmail.com>
2020-06-02 09:17:06 +0900

    Update BOJ 11052번 카드 구매하기
    문제링크 : https://www.acmicpc.net/problem/11052
    제출링크 : https://www.acmicpc.net/source/20145958
    문제풀이 : https://skysign.tistory.com/242

--- BOJ/11052번 카드 구매하기/src/Main.java
+++ BOJ/11052번 카드 구매하기/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * BOJ 11052번 카드 구매하기
  * 문제링크 : https://www.acmicpc.net/problem/11052
- * 제출링크 : https://www.acmicpc.net/source/20009261
+ * 제출링크 : https://www.acmicpc.net/source/20145958
  * 문제풀이 : https://skysign.tistory.com/242
  */
 public class Main {
@@ -20,15 +20,10 @@ public class Main {
 
         dp[1] = dt[1];
 
-        for(int i=2; i<=N; ++i) {
-            for(int j=1; j<=N; ++j) {
-                if(i-j > 0) {
-                    dp[i] = Math.max(dp[i-j] + dt[j], dp[i]);
-                }
+        for(int i=1; i<=N; ++i) {
+            for(int j=1; j<=i; ++j) {
+                dp[i] = Math.max(dp[i-j] + dt[j], dp[i]);
             }
-
-            if(i<=N)
-                dp[i] = Math.max(dp[i], dt[i]);
         }
 
         System.out.println(dp[N]);
