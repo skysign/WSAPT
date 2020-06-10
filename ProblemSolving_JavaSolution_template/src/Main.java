12f32be0a87512b3193fc6a4a37c6a0e84559535
ByeongKeon Kim <skysign@gmail.com>
2020-06-10 13:28:18 +0900

    ProblemSolving_JavaSolution_template

---
+++ ProblemSolving_JavaSolution_template/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    class Solution {
+        public int solution(int v) {
+            int answer = 0;
+
+            return answer;
+        }
+    }
+
+    public void slove() {
+        Solution sln = new Solution();
+        int v = 0;
+        int r = sln.solution(v);
+
+        System.out.println(r);
+    }
+
+    public static void main(String[] args) {
+	    Main main = new Main();
+	    main.slove()
+    }
+}
