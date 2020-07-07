36328c2c5dda5feab3b790fe01c7a3e831adbe67
ByeongKeon Kim <skysign@gmail.com>
2020-07-07 16:38:22 +0900

    BOJ 2156번 포도주 시식

---
+++ BOJ/2156번 포도주 시식/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public void solve() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        int[] dt = new int[n];
+        int[] dp = new int[n+1];
+
+        for (int i = 0; i < n; i++) {
+            dt[i] = sc.nextInt();
+        }
+
+        dp[1] = dt[0];
+
+        if(n <= 1) {
+            System.out.println(dp[n]);
+            return;
+        }
+
+        dp[2] = dp[1] + dt[1];
+
+        if(n <= 2) {
+            System.out.println(dp[n]);
+            return;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-3] + dt[i-2] + dt[i-1];
+            dp[i] = Math.max(dp[i], dp[i-2] + dt[i-1]);
+            dp[i] = Math.max(dp[i], dp[i-1]);
+        }
+
+        System.out.println(dp[n]);
+
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solve();
+    }
+}
