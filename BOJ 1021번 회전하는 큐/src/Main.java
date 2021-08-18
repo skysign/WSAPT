49c17d19b8d17deca97d3d12c47234f3effda0b3
ByeongKeon Kim <skysign@gmail.com>
2021-08-18 21:38:54 +0900

    BOJ 1021번 회전하는 큐

---
+++ BOJ 1021번 회전하는 큐/src/Main.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N, M;
+    int[] dt;
+    String[] strs;
+
+    ArrayList<Integer> que;
+
+    public void solve() throws IOException {
+        strs = br.readLine().split(" ");
+        N = Integer.parseInt(strs[0]);
+        M = Integer.parseInt(strs[1]);
+
+        dt = new int[M];
+        strs = br.readLine().split(" ");
+
+        for (int i = 0; i < M; ++i)
+            dt[i] = Integer.parseInt(strs[i]);
+
+        que = new ArrayList<>();
+
+        for (int i = 1; i <= N; ++i)
+            que.add(i);
+
+        int r = 0;
+        int left = 0;
+        int right = 0;
+
+        for (int m = 0; m < M; ++m) {
+            int target = dt[m];
+
+            for (int step = 0; step < que.size(); ++step) {
+                if (target == que.get(step)) {
+                    left = step;
+                    right = que.size() - step;
+                    break;
+                }
+            }
+
+            if (left < right) {
+                for (int i = 0; i < left; ++i) {
+                    que.add(que.get(0));
+                    que.remove(0);
+                }
+
+                r += left;
+            } else {
+                for (int i = 0; i < right; ++i) {
+                    que.add(0, que.get(que.size() - 1));
+                    que.remove(que.size() - 1);
+                }
+
+                r += right;
+            }
+
+            que.remove(0);
+        }
+
+        bw.write(String.valueOf(r));
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+    }
+}
