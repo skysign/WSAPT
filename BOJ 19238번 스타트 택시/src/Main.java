24fce637e5d520305df93399afc773b7cb6cce7a
ByeongKeon Kim <skysign@gmail.com>
2021-11-01 19:45:41 +0900

    * BOJ 1753번 최단경로
     * 유튜브 문제 풀이: https://youtu.be/Ks4Ox0jgOPQ
    
     * 문제링크: https://www.acmicpc.net/problem/19238
    
     * 자바소스: https://bit.ly/3nPAjmy

--- BOJ 19238번 스타트 택시/src/Main.java
+++ BOJ 19238번 스타트 택시/src/Main.java
@@ -1,6 +1,19 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 스터디에서 알고리즘 문제 함께 풀어보실 분들
+ * 여기로 → https://wsapt.github.io/public/
+ *
+ * BOJ 1753번 최단경로
+ *
+ * 유튜브 문제 풀이: https://youtu.be/Ks4Ox0jgOPQ
+ *
+ * 문제링크: https://www.acmicpc.net/problem/19238
+ *
+ * 자바소스: https://bit.ly/3nPAjmy
+ */
+
 public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
