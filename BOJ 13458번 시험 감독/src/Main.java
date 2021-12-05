757b60076beec2763bbfeb4bdb366769951ebbc5
ByeongKeon Kim <skysign@gmail.com>
2021-12-05 20:54:11 +0900

    * BOJ 13458번 시험 감독
     * 유튜브 문제 풀이: https://youtu.be/C8ElgcREo2w
    
     * 문제링크: https://www.acmicpc.net/problem/13458
    
     * 자바소스: https://bit.ly/3GhhfVY

--- BOJ 13458번 시험 감독/src/Main.java
+++ BOJ 13458번 시험 감독/src/Main.java
@@ -1,5 +1,18 @@
 import java.io.*;
 
+/**
+ * 스터디에서 알고리즘 문제 함께 풀어보실 분들
+ * 여기로 → https://wsapt.github.io/public/
+ *
+ * BOJ 13458번 시험 감독
+ *
+ * 유튜브 문제 풀이: https://youtu.be/C8ElgcREo2w
+ *
+ * 문제링크: https://www.acmicpc.net/problem/13458
+ *
+ * 자바소스: https://bit.ly/3GhhfVY
+ */
+
 public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
