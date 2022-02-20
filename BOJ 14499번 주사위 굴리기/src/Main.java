247d9145158cb20ff79eb483159b5278c1d492e5
ByeongKeon Kim <skysign@gmail.com>
2022-02-20 17:11:42 +0900

    * BOJ 14499번 주사위 굴리기  * 유튜브 문제 풀이: https://youtu.be/GVdO6R3-kVA  * 문제링크: https://www.acmicpc.net/problem/14499  * 자바소스: https://bit.ly/3BzihLW

--- BOJ 14499번 주사위 굴리기/src/Main.java
+++ BOJ 14499번 주사위 굴리기/src/Main.java
@@ -1,6 +1,19 @@
 import java.io.*;
 import java.util.Arrays;
 
+/**
+ * 스터디에서 알고리즘 문제 함께 풀어보실 분들
+ * 여기로 → https://wsapt.github.io/public/
+ *
+ * BOJ 14499번 주사위 굴리기
+ *
+ * 유튜브 문제 풀이: https://youtu.be/GVdO6R3-kVA
+ *
+ * 문제링크: https://www.acmicpc.net/problem/14499
+ *
+ * 자바소스: https://bit.ly/3BzihLW
+ */
+
 public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
