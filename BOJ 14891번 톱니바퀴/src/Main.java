68a847b8d007d5d2dd31eb2046d06c47e91972e7
ByeongKeon Kim <skysign@gmail.com>
2022-05-05 15:28:42 +0900

    BOJ 14891번 톱니바퀴

---
+++ BOJ 14891번 톱니바퀴/src/Main.java
@@ -0,0 +1,153 @@
+import java.io.*;
+
+public class Main {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int[][] tobni = new int[5][8];
+    int N;
+
+    String[] strs;
+
+    static int CLOCKWISE = 1;
+    static int COUNTER_CLOCKWISE = -1;
+
+    public void solve() throws IOException {
+        for (int i = 1; i < tobni.length; ++i) {
+            String str = br.readLine();
+
+            for (int j = 0; j < str.length(); ++j) {
+                char c = str.charAt(j);
+
+                if (c == '0') {
+                    tobni[i][j] = 0;
+                } else {
+                    tobni[i][j] = 1;
+                }
+            }
+        }
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; ++i) {
+            strs = br.readLine().split(" ");
+            int nTomni = Integer.parseInt(strs[0]);
+            int dir = Integer.parseInt(strs[1]);
+
+            rotateTobni(nTomni, 0, dir);
+        }
+
+        /**
+         *  07  00  01   07  00  01   07  00  01
+         *  06      02   06      02   06      02
+         *  05  04  03   05  04  03   05  04  03
+         */
+
+        int r = score();
+
+        bw.write(String.valueOf(r));
+        bw.newLine();
+        bw.close();
+    }
+
+    public int score() {
+        int r = 0;
+
+        if (tobni[1][0] == 1)
+            r += 1;
+
+        if (tobni[2][0] == 1)
+            r += 2;
+
+        if (tobni[3][0] == 1)
+            r += 4;
+
+        if (tobni[4][0] == 1)
+            r += 8;
+
+        return r;
+    }
+
+    public void rotateTobni(int nTobni, int dTobni, int dir) {
+        if (dTobni == 0) {
+            int d = -1;
+            int v = nTobni + d;
+
+            if ((1 <= v) && (v <= 4)) {
+                if (tobni[v][2] != tobni[nTobni][6]) {
+                    rotateTobni(v, d, (dir == CLOCKWISE) ? COUNTER_CLOCKWISE : CLOCKWISE);
+                }
+            }
+
+            d = 1;
+            v = nTobni + d;
+
+            if ((1 <= v) && (v <= 4)) {
+                if (tobni[nTobni][2] != tobni[v][6]) {
+                    rotateTobni(v, d, (dir == CLOCKWISE) ? COUNTER_CLOCKWISE : CLOCKWISE);
+                }
+            }
+
+            _rotateTobni(nTobni, dir);
+        } else if (dTobni == -1) {
+            int d = -1;
+            int v = nTobni + d;
+
+            if ((1 <= v) && (v <= 4)) {
+                if (tobni[v][2] != tobni[nTobni][6]) {
+                    rotateTobni(v, d, (dir == CLOCKWISE) ? COUNTER_CLOCKWISE : CLOCKWISE);
+                }
+            }
+
+            _rotateTobni(nTobni, dir);
+        } else if (dTobni == 1) {
+            int d = 1;
+            int v = nTobni + d;
+
+            if ((1 <= v) && (v <= 4)) {
+                if (tobni[nTobni][2] != tobni[v][6]) {
+                    rotateTobni(v, d, (dir == CLOCKWISE) ? COUNTER_CLOCKWISE : CLOCKWISE);
+                }
+            }
+
+            _rotateTobni(nTobni, dir);
+        } else {
+            // error
+        }
+    }
+
+    public void _rotateTobni(int nTobni, int dir) {
+        if (CLOCKWISE == dir) {
+            int t = tobni[nTobni][7];
+
+            tobni[nTobni][7] = tobni[nTobni][6];
+            tobni[nTobni][6] = tobni[nTobni][5];
+            tobni[nTobni][5] = tobni[nTobni][4];
+            tobni[nTobni][4] = tobni[nTobni][3];
+            tobni[nTobni][3] = tobni[nTobni][2];
+            tobni[nTobni][2] = tobni[nTobni][1];
+            tobni[nTobni][1] = tobni[nTobni][0];
+
+            tobni[nTobni][0] = t;
+        } else if (COUNTER_CLOCKWISE == dir) {
+            int t = tobni[nTobni][0];
+
+            tobni[nTobni][0] = tobni[nTobni][1];
+            tobni[nTobni][1] = tobni[nTobni][2];
+            tobni[nTobni][2] = tobni[nTobni][3];
+            tobni[nTobni][3] = tobni[nTobni][4];
+            tobni[nTobni][4] = tobni[nTobni][5];
+            tobni[nTobni][5] = tobni[nTobni][6];
+            tobni[nTobni][6] = tobni[nTobni][7];
+
+            tobni[nTobni][7] = t;
+        } else {
+            // error
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+    }
+}
