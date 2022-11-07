fa98f6717351e24a5147fa05c2a4f958268e25a5
ByeongKeon Kim <skysign@gmail.com>
2022-11-07 09:11:36 +0900

    백준 2355번 시그마

--- 백준 2355번 시그마/src/Main.java
+++ 백준 2355번 시그마/src/Main.java
@@ -4,15 +4,20 @@ public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    // 두 정수 A와 B가 주어졌을 때,
+    // A < B?  A > B?
+
+    // 두 정수 사이에 있는 수의 합을 구하는 프로그램을 작성하시오. 사이에 있는 수들은 A와 B도 포함한다.
+    // loop ->
+    // 공식을 사용해서 계산
+
+    // 첫째 줄에 두 정수 A, B가 주어진다. (-2,147,483,648 ≤ A, B ≤ 2,147,483,647)
+    // 2,147,483,646
+    // 2,147,483,647
+    // + integer overflow -> long
+
     long A, B;
     String[] strs;
-
-    // 두 정수 A와 B가 주어졌을 때,
-    // A, B가 주어진다. (-2,147,483,648 ≤ A, B ≤ 2,147,483,647)
-    // 2,147,483,647 -> long
-    // 2,147,483,647
-
-    // A 가 큰가? B 가 큰가? -> Math.min /max A < B
 
     public void solve() throws IOException {
         strs = br.readLine().split(" ");
@@ -22,7 +27,7 @@ public class Main {
         long a = Math.min(A, B);
         long b = Math.max(A, B);
 
-        // (처음의 수 + 끝의 수) * (수의 갯수) / 2
+        // (처음의 수 + 끝의 수) * (수의 개수) / 2
         long ans = (a + b) * (b - a +1) / 2;
 
         bw.write(String.valueOf(ans));
