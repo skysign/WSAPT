758fdef7a2f4b988da5bdbeb1723cd098603e013
ByeongKeon Kim <skysign@gmail.com>
2022-11-28 09:48:21 +0900

    2022 KAKAO TECH INTERNSHIP 문제 4번 등산 코스 정하기

--- 2022 KAKAO TECH INTERNSHIP 문제 4번 등산 코스 정하기/src/Main.java
+++ 2022 KAKAO TECH INTERNSHIP 문제 4번 등산 코스 정하기/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    class Edge {
+    class Edge implements Comparable<Edge> {
         public int fr, to, wt;
 
         public Edge(int i, int j, int w) {
@@ -10,89 +10,123 @@ public class Main {
             to = j;
             wt = w;
         }
-    }
 
-    class Passage{
-        public int to, intensity;
-
-        public Passage(int _to) {
-            to = _to;
-            intensity = 0;
-        }
-
-        public Passage(int _to, int in) {
-            to = _to;
-            intensity = in;
+        @Override
+        public int compareTo(Edge o) {
+            if (this.wt == o.wt)
+                return Integer.compare(this.to, o.to);
+            return Integer.compare(this.wt, o.wt);
         }
     }
 
-    // fr, edge
-    HashMap<Integer, ArrayList<Edge>> edges = new HashMap<>();
+    int[] map;
+
+    PriorityQueue<Edge> visit = new PriorityQueue<>();
     HashSet<Integer> visited = new HashSet<>();
     HashSet<Integer> summits = new HashSet<>();
+    HashMap<Integer, ArrayList<Edge>> edges = new HashMap<>();
 
     public int[] solution(int n, int[][] paths, int[] gates, int[] _summits) {
+        visit.clear();
         visited.clear();
         summits.clear();
         edges.clear();
+        map = new int[n + 1];
+        Arrays.fill(map, Integer.MAX_VALUE);
 
-        for (int s: _summits) {
+        Edge answer = new Edge(-1, -1, Integer.MAX_VALUE);
+
+        for (int s : _summits) {
             summits.add(s);
         }
 
-        Passage answer = new Passage(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        for (int[] p : paths) {
+            Edge e1 = new Edge(p[0], p[1], p[2]);
+            Edge e2 = new Edge(p[1], p[0], p[2]);
 
-        for (int[] p: paths) {
-            Edge e = new Edge(p[0], p[1], p[2]);
-            Edge e1 = new Edge(p[1], p[0], p[2]);
+            if (!edges.containsKey(p[0]))
+                edges.put(p[0], new ArrayList<Edge>());
+            if (!edges.containsKey(p[1]))
+                edges.put(p[1], new ArrayList<Edge>());
 
-            if (!edges.containsKey(p[0])) {
-                edges.put(p[0], new ArrayList<>());
-            }
-            if (!edges.containsKey(p[1])) {
-                edges.put(p[1], new ArrayList<>());
-            }
-
-            edges.get(p[0]).add(e);
-            edges.get(p[1]).add(e1);
+            edges.get(p[0]).add(e1);
+            edges.get(p[1]).add(e2);
         }
 
-        for(int gate: gates) {
-            Deque<Passage> queue = new ArrayDeque<>();
-            queue.add(new Passage(gate));
-            visited.clear();
+        for (int gate : gates) {
+            visit.addAll(edges.get(gate));
+            visited.add(gate);
+            map[gate] = 0;
+        }
 
-            while(!queue.isEmpty()) {
-                Passage psgFr = queue.pollFirst();
-                visited.add(psgFr.to);
+        while (!visit.isEmpty()) {
+            Edge e = visit.poll();
 
-                for (Edge e: edges.get(psgFr.to)) {
-                    if (visited.contains(e.to))
-                        continue;
+            if (visited.contains(e.to))
+                continue;
 
-                    Passage psgTo = new Passage(e.to, Math.max(psgFr.intensity, e.wt));
+            int intensity = Math.max(e.wt, map[e.fr]);
+            if (intensity < map[e.to]) {
+                map[e.to] = intensity;
 
-                    if (summits.contains(e.to)) {
-                        if (psgTo.intensity == answer.intensity) {
-                            if (psgTo.to < answer.to)
-                                answer = psgTo;
-                        }
-                        else if (psgTo.intensity < answer.intensity)
-                            answer = psgTo;
+                if (summits.contains(e.to)) {
+                    Edge eSummit = new Edge(0, e.to, intensity);
 
-                        continue;
-                    }
-                    else {
-                        queue.add(psgTo);
-                    }
+                    if (answer.compareTo(eSummit) > 0)
+                        answer = eSummit;
+
+                    continue;
                 }
             }
+
+            if (summits.contains(e.to)) {
+                continue;
+            }
+
+            visit.addAll(edges.get(e.to));
+            visited.add(e.to);
         }
 
-        return new int[]{answer.to, answer.intensity};
+//        for (int gate : gates) {
+//            pqVisit.clear();
+//            visited.clear();
+//            map = new int[n + 1];
+//            Arrays.fill(map, Integer.MAX_VALUE);
+//
+//            pqVisit.addAll(edges.get(gate));
+//            visited.add(gate);
+//            map[gate] = 0;
+//
+//            while (!pqVisit.isEmpty()) {
+//                Edge e = pqVisit.poll();
+//
+//                if (visited.contains(e.to))
+//                    continue;
+//
+//                int intensity = Math.max(e.wt, map[e.fr]);
+//                if (intensity < map[e.to]) {
+//                    map[e.to] = intensity;
+//
+//                    if (summits.contains(e.to)) {
+//                        Edge eSummit = new Edge(0, e.to, intensity);
+//
+//                        if (answer.compareTo(eSummit) > 0)
+//                            answer = eSummit;
+//
+//                        continue;
+//                    }
+//                }
+//
+//                pqVisit.addAll(edges.get(e.to));
+//                visited.add(e.to);
+//            }
+//        }
+
+        return new int[]{answer.to, answer.wt};
     }
 
     public static void main(String[] args) throws IOException {
-
+        Main main = new Main();
+//        main.test();
     }
 }
