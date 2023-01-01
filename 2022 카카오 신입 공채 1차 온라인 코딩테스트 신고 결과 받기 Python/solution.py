c9610558be304af7a070a3aa6bfa5e516611fd29
ByeongKeon Kim <skysign@gmail.com>
2023-01-01 15:46:56 +0900

    2022 카카오 신입 공채 1차 온라인 코딩테스트 신고 결과 받기 Python

--- 2022 카카오 신입 공채 1차 온라인 코딩테스트 신고 결과 받기 Python/solution.py
+++ 2022 카카오 신입 공채 1차 온라인 코딩테스트 신고 결과 받기 Python/solution.py
@@ -1,37 +1,35 @@
 def solution(id_list, report, k):
     # key: 신고한 user
-    # value : 신고 당한 user
-    user_reported = {}
-
-    # key: 신고 당한 user
-    # value : 신고한 user
-    reported_user = {}
+    # value: 신고 받은 users []
+    user2reported = {}
+    # key: 신고 받은 user
+    # value: 신고한 users []
+    reported2user = {}
 
     for i in range(len(report)):
         user, reported = report[i].split()
 
-        if not reported_user.__contains__(reported):
-            reported_user[reported] = []
+        if not user2reported.__contains__(user):
+            user2reported[user] = []
 
-        if not reported_user[reported].__contains__(user):
-            reported_user[reported].append(user)
+        if not user2reported[user].__contains__(reported):
+            user2reported[user].append(reported)
 
-        if not user_reported.__contains__(user):
-            user_reported[user] = []
+        if not reported2user.__contains__(reported):
+            reported2user[reported] = []
 
-        if not user_reported[user].__contains__(reported):
-            user_reported[user].append(reported)
-
+        if not reported2user[reported].__contains__(user):
+            reported2user[reported].append(user)
 
     answer = [0 for _ in range(len(id_list))]
 
     for i in range(len(id_list)):
         user = id_list[i]
 
-        if user_reported.__contains__(user):
-            for r in user_reported[user]:
-                if reported_user.__contains__(r):
-                    if len(reported_user[r]) >= k:
+        if user2reported.__contains__(user):
+            for reported in user2reported[user]:
+                if reported2user.__contains__(reported):
+                    if len(reported2user[reported]) >= k:
                         answer[i] += 1
 
     return answer
--- 2022 카카오 신입 공채 1차 온라인 코딩테스트 신고 결과 받기 Python/test_solution.py
+++ 2022 카카오 신입 공채 1차 온라인 코딩테스트 신고 결과 받기 Python/test_solution.py
@@ -5,7 +5,11 @@ from solution import solution
 class Test(TestCase):
     def test_solution(self):
         self.assertEqual([2,1,1,0],
-                         solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
+                         solution(["muzi", "frodo", "apeach", "neo"],
+                                  ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],
+                                  2))
     def test_solution2(self):
         self.assertEqual([0,0],
-                         solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
+                         solution(["con", "ryan"],
+                                  ["ryan con", "ryan con", "ryan con", "ryan con"],
+                                  3))
