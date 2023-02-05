d577d38023a3359f80bde47e06613cdb8f2189f4
ByeongKeon Kim <skysign@gmail.com>
2023-02-06 08:37:35 +0900

    2022 카카오 신입 공채 1차 온라인 코딩테스트 k진수에서 소수 개수 구하기

--- 2022 카카오 신입 공채 1차 온라인 코딩테스트 k진수에서 소수 개수 구하기 Python/solution.py
+++ 2022 카카오 신입 공채 1차 온라인 코딩테스트 k진수에서 소수 개수 구하기 Python/solution.py
@@ -1,50 +1,55 @@
 import string
 
+# x 약수 <= y
+# y * y <= x
 
-def findN2(max_number):
+def findN2(number):
     n = 1
 
-    while n * n <= max_number:
+    while n * n <= number:
         n += 1
 
     return n
 
-
-def isPrimeNumber(number):
+def IsPrimeNumber(number):
     if number <= 1:
         return False
 
     n2 = findN2(number)
 
-    for n in range(2, n2 + 1):
+    for n in range(2, n2):
         if number % n == 0 and n != number:
-            return False  # number가 n 의 배수 임으로 not prime number
+            return False
 
-    return True
+    return True;
 
-
+# 0~~~~9 a~~z
 hexcode = string.digits + string.ascii_lowercase
 
-
-def convert_jinbum_10_to_base(number10, base):
-    q, r = divmod(number10, base)
+def convert_10_to_k(n, k):
+    # n / k = q, r
+    # 5 / 2 = 2 .. 1
+    # 2 / 2 = 1 .. 0
+    # 1 / 2 = 0 .. 1
+    # 101 2진법 = 5 10진법
+    q, r = divmod(n, k)
 
     if q == 0:
         return hexcode[r]
 
-    return convert_jinbum_10_to_base(q, base) + hexcode[r]
-
+    return convert_10_to_k(q, k) + hexcode[r]
 
 def solution(n, k):
     answer = 0
-    number = convert_jinbum_10_to_base(n, k)
-    number = str(number)
+    # 10진법의 수가 -> k 진법으로 변환
+    number = convert_10_to_k(n, k)
 
+    # 123412350000000023512351235
     for n in number.split('0'):
         if len(n) <= 0:
             continue
 
-        if isPrimeNumber(int(n)):
+        if IsPrimeNumber(int(n)):
             answer += 1
 
     return answer
