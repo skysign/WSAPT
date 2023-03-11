50cce2d124f8e27b4c92f8b260dedece0f0dfac9
ByeongKeon Kim <skysign@gmail.com>
2023-03-11 15:32:56 +0900

    # 2023 카카오 신입 공채 1차 온라인 코딩 테스트 개인정보 수집 유효기간 Python
    # 유튜브 문제 풀이: https://youtu.be/lZa2qBb9Sts
    
    # 파이썬 소스: http://bit.ly/3FbQzIc
    
    # 문제 링크: http://bit.ly/3YD8XR9

--- 2023 카카오 신입 공채 1차 온라인 코딩 테스트 개인정보 수집 유효기간 Python/main.py
+++ 2023 카카오 신입 공채 1차 온라인 코딩 테스트 개인정보 수집 유효기간 Python/main.py
@@ -1,3 +1,11 @@
+# 2023 카카오 신입 공채 1차 온라인 코딩 테스트 개인정보 수집 유효기간 Python
+#
+# 유튜브 문제 풀이: https://youtu.be/lZa2qBb9Sts
+#
+# 파이썬 소스: http://bit.ly/3FbQzIc
+#
+# 문제 링크: http://bit.ly/3YD8XR9
+
 def myDays(ymd):
     y, m, d = ymd.split('.')
     y = int(y)
