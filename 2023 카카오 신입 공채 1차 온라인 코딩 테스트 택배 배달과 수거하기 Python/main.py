397f19cd6e738e059d2de9bb65ea84b50ee8d0f5
ByeongKeon Kim <skysign@gmail.com>
2023-04-02 20:47:21 +0900

    # 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python
    # 유튜브 문제 풀이: https://youtu.be/5FX8K-bih9Q
    
    # 파이썬 소스: http://bit.ly/40ycouk
    
    # 문제 링크: http://bit.ly/3MaqmxT

--- 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python/main.py
+++ 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python/main.py
@@ -1,3 +1,11 @@
+# 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python
+#
+# 유튜브 문제 풀이: https://youtu.be/5FX8K-bih9Q
+#
+# 파이썬 소스: http://bit.ly/40ycouk
+#
+# 문제 링크: http://bit.ly/3MaqmxT
+
 def solution(cap, n, deliveries, pickups):
     answer = 0
     deliveries_remained_cap = 0
--- 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python/test_main.py
+++ 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python/test_main.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 from main import solution
 
+# 2023 카카오 신입 공채 1차 온라인 코딩 테스트 택배 배달과 수거하기 Python
+#
+# 유튜브 문제 풀이: https://youtu.be/5FX8K-bih9Q
+#
+# 파이썬 소스: http://bit.ly/40ycouk
+#
+# 문제 링크: http://bit.ly/3MaqmxT
+
 class Test(TestCase):
     def test_solution(self):
         self.assertEqual(16,
