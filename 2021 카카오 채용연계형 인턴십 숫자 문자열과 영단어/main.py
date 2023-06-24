8e1507268c1b0883622a44ba3a36bab077fc931c
ByeongKeon Kim <skysign@gmail.com>
2023-06-24 17:37:29 +0900

    # 2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어
    # 유튜브 문제 풀이: https://youtu.be/gN6aK7l1chg
    
    # 파이썬 소스: https://bit.ly/3CMOuky
    
    # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/81301

--- 2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어/main.py
+++ 2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어/main.py
@@ -1,3 +1,11 @@
+# 2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어
+#
+# 유튜브 문제 풀이: https://youtu.be/gN6aK7l1chg
+#
+# 파이썬 소스: https://bit.ly/3CMOuky
+#
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
 def solution(str_input):
     str_numbers = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ]
 
