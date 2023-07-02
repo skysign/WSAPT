0e2013a51d369a0d319a806a847aaaf955e4d19f
ByeongKeon Kim <skysign@gmail.com>
2023-07-02 20:13:26 +0900

    2021 카카오 채용연계형 인턴십 거리두기 확인하기 Python

---
+++ 2021 카카오 채용연계형 인턴십 거리두기 확인하기/main.py
@@ -0,0 +1,57 @@
+# 2021 카카오 채용연계형 인턴십 거리두기 확인하기 Python
+#
+# 유튜브 문제 풀이:
+#
+# 파이썬 소스:
+#
+# 문제 링크:
+
+def solution(places):
+    answer = []
+
+    for place55 in places:
+        answer.append(get_answer(place55))
+
+    return answer
+
+
+def get_answer(place):
+    pijs = []
+
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == "P":
+                pijs.append([i, j])
+
+    for pij in pijs:
+        visited = [[False for _ in range(5)] for _ in range(5)]
+        if 0 == bfs([pij], place, visited, depth=2):
+            return 0
+
+    return 1
+
+
+def bfs(queue, place, visited, depth):
+    dijs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    depth -= 1
+    new_queue = []
+
+    while len(queue) > 0:
+        si, sj = queue.pop(0)
+        visited[si][sj] = True
+
+        for dij in dijs:
+            di, dj = dij
+            dsti = si + di
+            dstj = sj + dj
+
+            if 0 <= dsti < 5 and 0 <= dstj < 5 and visited[dsti][dstj] == False:
+                if place[dsti][dstj] == "P":
+                    return 0
+                elif place[dsti][dstj] == "O":
+                    new_queue.append([dsti, dstj])
+
+    if depth > 0:
+        return bfs(new_queue, place, visited, depth)
+
+    return 1
---
+++ 2021 카카오 채용연계형 인턴십 거리두기 확인하기/test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from main import solution
+
+class Test(TestCase):
+    def test1_solution(self):
+        self.assertEqual(
+            [1, 0, 1, 1, 1],
+            solution(
+                [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+            )
+        )
