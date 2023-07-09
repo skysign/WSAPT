72a4beb5d01fd8ac1779139c5ae23953fb4a2a13
ByeongKeon Kim <skysign@gmail.com>
2023-07-09 20:35:31 +0900

    # 2021 카카오 채용연계형 인턴십 표 편집
    # 유튜브 문제 풀이: https://youtu.be/mlJu7Y4kg0o
    
    # 파이썬 소스: https://bit.ly/3NBBIu4
    
    # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/81303

--- 2021 카카오 채용연계형 인턴십 표 편집/test_main.py
+++ 2021 카카오 채용연계형 인턴십 표 편집/test_main.py
@@ -1,3 +1,11 @@
+# 2021 카카오 채용연계형 인턴십 표 편집
+#
+# 유튜브 문제 풀이: https://youtu.be/mlJu7Y4kg0o
+#
+# 파이썬 소스: https://bit.ly/3NBBIu4
+#
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/81303
+
 from unittest import TestCase
 from main import solution
 
