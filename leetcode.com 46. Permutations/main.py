39ebf6c17ce0fe73970df1d7a86ce54684724f0d
ByeongKeon Kim <skysign@gmail.com>
2023-09-03 08:53:22 +0900

    leetcode.com 46. Permutations

--- leetcode.com 46. Permutations/main.py
+++ leetcode.com 46. Permutations/main.py
@@ -1,31 +1,33 @@
 import copy
 from typing import List
 
+
+def permutation(nums: List[int], length: int, path: List[int], answer: List[List[int]]):
+    visited = [False for _ in range(len(nums))]
+
+    def rec(path):
+        if len(path) == length:
+            answer.append(copy.deepcopy(path))
+            return
+
+        for idx in range(len(nums)):
+            if not visited[idx]:
+                visited[idx] = True
+                path.append(nums[idx])
+                rec(path)
+                path.pop(len(path) -1)
+                visited[idx] = False
+
+    rec(path)
+
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         answer = []
-        visited = [False for _ in range(len(nums))]
-        self.permutation(nums, len(nums), answer)
+        nums.sort()
+
+        permutation(nums, len(nums), [], answer)
 
         return answer
 
-    def permutation(self, nums: List[int], length: int, answer: List[List[int]]):
-        nums.sort()
-        visited = [False for _ in range(len(nums))]
 
-        def rec(path: List[int], visited: List[int]):
-            if len(path) == length:
-                answer.append(copy.deepcopy(path))
-                return
-
-            for idx in range(len(nums)):
-                if not visited[idx]:
-                    num = nums[idx]
-                    path.append(num)
-                    visited[idx] = True
-                    rec(path, visited)
-                    visited[idx] = False
-                    path.pop(len(path) -1)
-
-
-        rec([], visited)
