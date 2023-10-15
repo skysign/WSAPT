24475766b38c12f275097233545c0e3fdbf8a1e3
ByeongKeon Kim <skysign@gmail.com>
2023-10-15 09:16:57 +0900

    # leetcode.com 15. 3Sum
    # 유튜브 문제 풀이: https://youtu.be/RCa2xJFDUIg?si=BR9FHxPXvIM30adZ
    
    # 파이썬 소스: https://bit.ly/3LRMvQR
    
    # 문제 링크: https://leetcode.com/problems/3sum/

--- leetcode.com 15. 3Sum/main.py
+++ leetcode.com 15. 3Sum/main.py
@@ -1,5 +1,13 @@
 from typing import List
 
+# leetcode.com 15. 3Sum
+#
+# 유튜브 문제 풀이: https://youtu.be/RCa2xJFDUIg?si=BR9FHxPXvIM30adZ
+#
+# 파이썬 소스: https://bit.ly/3LRMvQR
+#
+# 문제 링크: https://leetcode.com/problems/3sum/
+
 class Solution:
     def threeSum(self, dt: List[int]) -> List[List[int]]:
         myset = set()
--- leetcode.com 15. 3Sum/test_main.py
+++ leetcode.com 15. 3Sum/test_main.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 from main import Solution
 
+# leetcode.com 15. 3Sum
+#
+# 유튜브 문제 풀이: https://youtu.be/RCa2xJFDUIg?si=BR9FHxPXvIM30adZ
+#
+# 파이썬 소스: https://bit.ly/3LRMvQR
+#
+# 문제 링크: https://leetcode.com/problems/3sum/
+
 class TestSolution(TestCase):
     def test1_three_sum(self):
         sol = Solution()
