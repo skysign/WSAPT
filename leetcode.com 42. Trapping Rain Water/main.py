358a8fd422a292c7e66b582861b071ee59faf9a2
ByeongKeon Kim <skysign@gmail.com>
2023-10-18 07:43:59 +0900

    leetcode.com 42. Trapping Rain Water

--- leetcode.com 42. Trapping Rain Water/main.py
+++ leetcode.com 42. Trapping Rain Water/main.py
@@ -1,10 +1,52 @@
 from typing import List
 
 class Solution:
+    def trap(self, heights: List[int]) -> int:
+        rlt = 0
+
+        n = len(heights)
+
+        heights_lt_max = 0
+        heights_lt = [0] * n
+
+        heights_rt_max = 0
+        heights_rt = [0] * n
+
+        for idx in range(0, len(heights)):
+            heights_lt_max = max(heights_lt_max, heights[idx])
+            heights_lt[idx] = heights_lt_max
+
+        for idx in range(len(heights) -1, -1, -1):
+            heights_rt_max = max(heights_rt_max, heights[idx])
+            heights_rt[idx] = heights_rt_max
+
+        for pmid in range(1, len(heights) -1):
+            height_lt = heights_lt[pmid]
+            height_rt = heights_rt[pmid]
+            height_min = min(height_lt, height_rt)
+
+            if height_min - heights[pmid] > 0:
+                rlt += (height_min - heights[pmid])
+
+        return rlt
+
+    def trap3(self, heights: List[int]) -> int:
+        rlt = 0
+
+        for pmid in range(1, len(heights) -1):
+            height_lt = max(heights[0:pmid])
+            height_rt = max(heights[pmid +1:len(heights)])
+            height_min = min(height_lt, height_rt)
+
+            if height_min - heights[pmid] > 0:
+                rlt += (height_min - heights[pmid])
+
+        return rlt
+
     def __init__(self):
         self.rlt = 0
 
-    def trap(self, heights: List[int]) -> int:
+    def trap2(self, heights: List[int]) -> int:
         # for plt in range(len(heights)):
         plt = 0
         while plt < len(heights) -1:
@@ -13,9 +55,55 @@ class Solution:
                 plt += 1
                 continue
 
-            (found, max_height_rt) = self.find_my_height_pair(plt, height_lt, heights)
+            [found, max_height_rt] = self.find_my_height_pair2(plt, height_lt, heights)
+
+            if found != None and max_height_rt != 0:
+                plt = found
+            else:
+                plt += 1
+
+        return self.rlt
+
+    def find_my_height_pair2(self, plt, height_lt, heights):
+        minus_area = 0
+        max_height_rt = 0
+        prt_max = 0
+
+        if height_lt == heights[plt +1]:
+            return [None, 0]
+
+        for prt in range(plt + 1, len(heights)):
+            if max_height_rt < heights[prt]:
+                max_height_rt = heights[prt]
+                prt_max = prt
+
+                if max_height_rt >= height_lt:
+                    break
+
+        our_height = min(height_lt, max_height_rt)
+
+        for idx in range(plt + 1, prt_max):
+            if heights[idx] < our_height:
+                minus_area += heights[idx]
+
+        self.rlt += ((prt_max - plt -1) * our_height)
+        self.rlt -= minus_area
+
+        return [prt_max, max_height_rt]
+
+
+    def trap1(self, heights: List[int]) -> int:
+        # for plt in range(len(heights)):
+        plt = 0
+        while plt < len(heights) -1:
+            height_lt = heights[plt]
+            if height_lt <= 0:
+                plt += 1
+                continue
+
+            (found, max_height_rt) = self.find_my_height_pair1(plt, height_lt, heights)
             if found == None:
-                (found, max_height_rt) = self.find_my_height_pair(plt, max_height_rt, heights)
+                (found, max_height_rt) = self.find_my_height_pair1(plt, max_height_rt, heights)
 
             if found != None:
                 plt = found
@@ -24,7 +112,7 @@ class Solution:
 
         return self.rlt
 
-    def find_my_height_pair(self, plt, height_lt, heights):
+    def find_my_height_pair1(self, plt, height_lt, heights):
         minus_area = 0
         max_height_rt = 0
 
--- leetcode.com 42. Trapping Rain Water/test_main.py
+++ leetcode.com 42. Trapping Rain Water/test_main.py
@@ -10,6 +10,22 @@ class TestSolution(TestCase):
         sol = Solution()
         self.assertEqual(9, sol.trap([4,2,0,3,2,5]))
 
+    def test4_trap(self):
+        sol = Solution()
+        self.assertEqual(0, sol.trap([0, 2, 0]))
+
     def test145_trap(self):
         sol = Solution()
         self.assertEqual(1, sol.trap([4,2,3]))
+
+    def test160_trap(self):
+        sol = Solution()
+        self.assertEqual(23, sol.trap([5,5,1,7,1,1,5,2,7,6]))
+
+    def test169_trap(self):
+        sol = Solution()
+        self.assertEqual(83, sol.trap([6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3]))
+
+    def test263_trap(self):
+        sol = Solution()
+        self.assertEqual(23, sol.trap([0,5,6,4,6,1,0,0,2,7]))
