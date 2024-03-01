2c4f57a5beb0d4da8b33fdea9c5538a0d53c4f71
ByeongKeon Kim <skysign@gmail.com>
2024-03-01 16:24:54 +0900

    programmers.co.kr 코딩테스트 연습 힙(Heap) 디스크 컨트롤러
    문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42627
    
    파이썬 소스: https://bit.ly/4bNyGhw

--- programmers.co.kr 코딩테스트 연습 힙(Heap) 디스크 컨트롤러/main.py
+++ programmers.co.kr 코딩테스트 연습 힙(Heap) 디스크 컨트롤러/main.py
@@ -1,11 +1,12 @@
 from typing import List
 import heapq
+from functools import cmp_to_key
 
 def solution(jobs: List):
     current_time = 0
     answer = 0
     count = len(jobs)
-    jobs.sort(key=lambda x: x[0])
+    jobs = sorted(jobs, key=cmp_to_key(my_sort))
     heap = []
 
     while len(jobs) > 0 or len(heap) > 0:
@@ -28,3 +29,16 @@ def solution(jobs: List):
             current_time += 1
 
     return int(answer / count)
+
+def my_sort(a, b):
+    if a[0] < b[0]:
+        return -1
+    elif a[0] > b[0]:
+        return 1
+    else: # a[0] == b[0]
+        if a[1] < b[1]:
+            return -1
+        elif a[1] > b[1]:
+            return 1
+        else: # a[1] == b[1]
+            return 0
