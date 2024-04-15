d9d25d77df701f4a175d81a7a65ad3d6a26eaca1
ByeongKeon Kim <skysign@gmail.com>
2024-04-15 18:38:56 +0900

    2023 KAKAO BLIND RECRUITMENT 표현 가능한 이진트리 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/150367

--- 2023 KAKAO BLIND RECRUITMENT 표현 가능한 이진트리/main.py
+++ 2023 KAKAO BLIND RECRUITMENT 표현 가능한 이진트리/main.py
@@ -18,26 +18,26 @@ def check_binarytree(n: int) -> int:
         else:
             return 0
 
-
     l, length = 2, 0
 
     while l <= len(b):
         l *= 2
-        length = l -1
+        length = l - 1
 
     begin0 = ['0'] * (length - len(b))
     b = ''.join(begin0) + b
 
     return check(b)
 
+
 def check(b: str) -> int:
     if len(b) == 1:
         return 1
 
     half = len(b) // 2
-    parent = b[half:half+1]
+    parent = b[half:half + 1]
     left = b[:half]
-    right = b[half+1:]
+    right = b[half + 1:]
 
     if parent == '0':
         if left.find('1') != -1:
