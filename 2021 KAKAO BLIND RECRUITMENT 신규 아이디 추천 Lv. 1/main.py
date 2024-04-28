db58294118a00c9184bbd81f83d401373953fb3f
ByeongKeon Kim <skysign@gmail.com>
2024-04-28 18:22:24 +0900

    2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 Lv. 1 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/72410

--- 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 Lv. 1/main.py
+++ 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 Lv. 1/main.py
@@ -7,7 +7,6 @@ def solution(new_id: str):
     id = new_id.lower()
 
     # 2단계
-    id: List = list(id)
     new_id = []
     for c in id:
         if c in '0123456789abcdefghijklmnopqrstuvwxyz-_.':
@@ -18,7 +17,7 @@ def solution(new_id: str):
     id = re.sub(r'\.+', '.', id)
 
     # 4단계
-    if id[0] == '.':
+    if len(id) > 0 and id[0] == '.':
         id = id[1:]
 
     if len(id) > 0 and id[len(id) - 1] == '.':
