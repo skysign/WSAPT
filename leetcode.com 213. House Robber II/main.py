a816086a3ec800f97c50b15ee029ac7c70f98032
ByeongKeon Kim <skysign@gmail.com>
2024-06-01 18:59:51 +0900

    leetcode.com 213. House Robber II 문제 링크: https://leetcode.com/problems/house-robber-ii/description/

--- leetcode.com 213. House Robber II/main.py
+++ leetcode.com 213. House Robber II/main.py
@@ -3,12 +3,13 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
+        l = len(nums)
+        if l <= 2:
             return max(nums)
 
-        dp0 = [0 for _ in range(len(nums))]
-        dp1 = [0 for _ in range(len(nums))]
-        dp2 = [0 for _ in range(len(nums))]
+        dp0 = [0 for _ in range(l)]
+        dp1 = [0 for _ in range(l)]
+        dp2 = [0 for _ in range(l)]
 
         dp0[0] = nums[0]
         dp0[1] = 0
@@ -27,13 +28,13 @@ class Solution:
 
         dp0[2] = dp0[0] + nums[2]
 
-        for i in range(3, len(nums) - 1):
+        for i in range(3, l - 1):
             dp0[i] = max(dp0[i - 2], dp0[i - 3]) + nums[i]
             dp1[i] = max(dp1[i - 2], dp1[i - 3]) + nums[i]
             dp2[i] = max(dp2[i - 2], dp2[i - 3]) + nums[i]
 
-        i = len(nums) - 1
+        i = l - 1
         dp1[i] = max(dp1[i - 2], dp1[i - 3]) + nums[i]
         dp2[i] = max(dp2[i - 2], dp2[i - 3]) + nums[i]
 
-        return max(max(dp0), max(dp1), max(dp2))
+        return max(max(dp0[l-3:l]), max(dp1[l-3:l]), max(dp2[l-3:l]))
