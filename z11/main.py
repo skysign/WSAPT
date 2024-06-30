8b2c50c537fb5dddd243e73ce7c2dcc6266a20c6
ByeongKeon Kim <skysign@gmail.com>
2024-06-30 19:37:09 +0900

    z11 test 3

--- z11/main.py
+++ z11/main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
