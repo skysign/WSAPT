268fbde9c8f3928912c4008079ff41f4a824d6e3
ByeongKeon Kim <skysign@gmail.com>
2024-06-30 19:10:50 +0900

    z10 test 5

--- z10/main.py
+++ z10/main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     asdfasdf asdfasdf asdfasdf asdfasdfasdf asdfasd
-
+asdfasdff
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
