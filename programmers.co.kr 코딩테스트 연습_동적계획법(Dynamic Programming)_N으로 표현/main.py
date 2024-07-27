df14cfa7d7ca305d79d0edefc4f757e38d37f022
ByeongKeon Kim <skysign@gmail.com>
2024-07-27 16:12:36 +0900

    programmers.co.kr 코딩테스트 연습_동적계획법(Dynamic Programming)_N으로 표현 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42895

--- programmers.co.kr 코딩테스트 연습_동적계획법(Dynamic Programming)_N으로 표현/main.py
+++ programmers.co.kr 코딩테스트 연습_동적계획법(Dynamic Programming)_N으로 표현/main.py
@@ -1,82 +1,31 @@
-import copy
-from collections import defaultdict
-from typing import List, Dict
+from typing import List, Set
 
 
-def solution(N: int, number: int):
-    # X / N 의 값이 number의 최소값이 될 수 있음
-    # 따라서, number * N + 1 이 dp의 최소 길이
-    l = number * N + 1
-    dp: List[int] = [9] * l
+def solution(N, number):
+    if N == number:
+        return 1
 
-    dp[N] = 1
-    dp[1] = min(dp[1], 2)  # N/N, N이 1인 경우도 고려해서, min()이 필요함
+    dp: List[Set] = [set() for _ in range(9)]
 
-    # NN, NNN등의 숫자 처리필요함
-    str = '{:d}'.format(N)
-    cntN = 1
+    strN = '{:d}'.format(N)
 
-    while int(str) <= l:
-        dp[int(str)] = min(dp[int(str)], cntN)
-        str += '{:d}'.format(N)
-        cntN += 1
+    for i in range(1, len(dp)):
+        st = dp[i]
+        st.add(int(''.join(([strN] * i))))
 
-    def do_plus():
-        for i in range(l - 1, 0, -1):
-            if dp[i] <= 8 and 1 <= i + N < l:
-                dp[i + N] = min(dp[i + N], dp[i] + 1)
+    for i in range(2, 9):
+        for j in range(1, i):
+            for v1 in dp[j]:
+                for v2 in dp[i-j]:
+                    dp[i].add(v1 + v2)
+                    dp[i].add(v1 - v2)
+                    dp[i].add(v1 * v2)
 
-    def do_multiply():
-        for i in range(l - 1, 0, -1):
-            if dp[i] <= 8 and 1 <= i * N < l:
-                dp[i * N] = min(dp[i * N], dp[i] + 1)
+                    if v2 != 0:
+                        dp[i].add(v1 // v2)
 
-    def do_minus():
-        for i in range(1, l):
-            if dp[i] <= 8 and 1 <= i - N < l:
-                dp[i - N] = min(dp[i - N], dp[i] + 1)
+        if number in dp[i]:
+            return i
+            break
 
-    def do_divide():
-        for i in range(1, l):
-            if dp[i] <= 8 and 1 <= i // N < l:
-                dp[i // N] = min(dp[i // N], dp[i] + 1)
-
-    for _ in range(8):
-        do_plus()
-        do_multiply()
-        do_minus()
-        do_divide()
-
-    dict: Dict = defaultdict(lambda: 9)
-
-    for i in range(1, l):
-        if dp[i] > 8:
-            continue
-        else:
-            dict[i] = dp[i]
-
-    dict_local: Dict = copy.deepcopy(dict)
-    li = list(dict.keys())
-    lj = list(dict.keys())
-    li.sort()
-    lj.sort()
-
-    for i in li:
-        for j in lj:
-            if i == j:
-                continue
-
-            vs = [i + j, i * j, i // j, i - j,
-                  j + i, j * i, j // i, j - i]
-            for v in vs:
-                if 1 <= v < l:
-                    dict_local[v] = min(dict_local[v], dict[i] + dict[j])
-                    if dict_local[v] > 8:
-                        del dict_local[v]
-
-    dict = dict_local
-
-    if dict[number] > 8:
-        return -1
-
-    return dict[number]
+    return -1
