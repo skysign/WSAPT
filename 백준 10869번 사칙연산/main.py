6804cb8809c33db0db3e1e2ec6bf48ce81960117
ByeongKeon Kim <skysign@gmail.com>
2024-08-05 19:39:12 +0900

    백준 10869번 사칙연산 문제 링크: https://www.acmicpc.net/problem/10869

---
+++ 백준 10869번 사칙연산/main.py
@@ -0,0 +1,11 @@
+def solve():
+    a, b = input().split(' ')
+    a, b = int(a), int(b)
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a // b)
+    print(a % b)
+
+if __name__ == '__main__':
+    solve()
