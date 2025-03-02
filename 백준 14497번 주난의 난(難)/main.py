f4aab7c01d8c20e2caba38530148abf855e9481b
ByeongKeon Kim <skysign@gmail.com>
2025-03-02 15:58:59 +0900

    백준 14497번 주난의 난(難) 문제 링크: https://www.acmicpc.net/problem/14497

--- 백준 14497번 주난의 난(難)/main.py
+++ 백준 14497번 주난의 난(難)/main.py
@@ -7,23 +7,14 @@ def solve():
     N, M = map(int, input().strip().split())
     junan_y, junan_x, thief_y, thief_x = map(int, input().strip().split())
     junan_y, junan_x, thief_y, thief_x = junan_y - 1, junan_x - 1, thief_y - 1, thief_x - 1
-    maps = []
-
-
-    for _ in range(N):
-        maps.append(list(input().strip()))
-
-    queue = deque([[junan_y, junan_x]])
-
-    dys = [-1, 0, 1, 0]
-    dxs = [0, 1, 0, -1]
-
+    maps = [list(input().strip()) for _ in range(N)]
     answer = 1
     students = deque()
 
     while True:
         visited = [[False for _ in range(M)] for _ in range(N)]
 
+        queue = deque([[junan_y, junan_x]])
         while queue:
             sy, sx = queue.popleft()
             if visited[sy][sx]:
@@ -31,8 +22,8 @@ def solve():
 
             visited[sy][sx] = True
 
-            for i in range(len(dys)):
-                dy, dx = dys[i], dxs[i]
+            dyxs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+            for dy, dx in dyxs:
                 y, x = sy + dy, sx + dx
 
                 if 0 <= y < N and 0 <= x < M and visited[y][x] == False:
@@ -49,8 +40,6 @@ def solve():
             maps[y][x] = 0
 
         answer += 1
-
-        queue.append([junan_y, junan_x])
 
 
 if __name__ == '__main__':
