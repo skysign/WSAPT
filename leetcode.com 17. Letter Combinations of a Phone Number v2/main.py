74abf06477c460d7b646c1f06765990cc9bea5ef
ByeongKeon Kim <skysign@gmail.com>
2025-04-13 10:12:32 +0900

    leetcode.com 17. Letter Combinations of a Phone Number 문제 링크: https://leetcode.com/problems/letter-combinations-of-a-phone-number

---
+++ leetcode.com 17. Letter Combinations of a Phone Number v2/main.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dict = {}
+        dict['2'] = ['a', 'b', 'c']
+        dict['3'] = ['d', 'e', 'f']
+        dict['4'] = ['g', 'h', 'i']
+        dict['5'] = ['j', 'k', 'l']
+        dict['6'] = ['m', 'n', 'o']
+        dict['7'] = ['p', 'q', 'r', 's']
+        dict['8'] = ['t', 'u', 'v']
+        dict['9'] = ['w', 'x', 'y', 'z']
+
+        queue = [['']]
+        queue2 = []
+
+        for d in digits:
+            for prev in queue:
+                for new in dict[d]:
+                    queue2.append(prev + [new])
+
+            queue = queue2
+            queue2 = []
+
+        answer = []
+        for q in queue:
+            a = ''.join(q)
+            if a != '':
+                answer.append(a)
+
+        return answer
