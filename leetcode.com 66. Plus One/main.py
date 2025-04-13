8aafd138e85ea0d43f33228b54ce13fad1a7c363
ByeongKeon Kim <skysign@gmail.com>
2025-04-13 20:00:30 +0900

    leetcode.com 66. Plus One 문제 링크: https://leetcode.com/problems/plus-one

---
+++ leetcode.com 66. Plus One/main.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 0
+        answer = []
+        digits.reverse()
+        digits[0] += 1
+
+        for d in digits:
+            d += carry
+
+            if d >= 10:
+                carry = 1
+                d = 0
+            else:
+                carry = 0
+
+            answer.append(d)
+
+        if carry > 0:
+            answer.append(carry)
+
+        answer.reverse()
+
+        return answer
