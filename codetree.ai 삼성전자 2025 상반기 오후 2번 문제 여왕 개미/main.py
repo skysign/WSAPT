e133aed72d275b9451dff342072e7a51e9a6465d
ByeongKeon Kim <skysign@gmail.com>
2026-04-16 19:23:11 +0900

    codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미 문제 링크: https://www.codetree.ai/ko/frequent-problems/all/problems/queen-ant

--- codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미/main.py
+++ codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미/main.py
@@ -15,10 +15,25 @@ def solve():
             houses[q] = -1
         elif cmds[0] == 400:
             r = cmds[1]
+
+            if r >= len(houses) - 1:
+                print(0)
+                continue
+
+
             mn_time = 0
-            # mx_time = houses[-1]
-            mx_time = 1000000000
             min_time = 0
+
+            # 문제 푸는 방법은 2가지가 있는대요,
+            # #1 철거되지 않은 모든 개미집을 모아서
+            # 가장 오른쪽에 있는 개미집의 위치를 mx_time으로 사용하는 방법
+            # 최대값인 1000000000 보다 작은 값을 사용하게 되어서 좀더 빠른 속도를 기대할 수 있지만,
+            # 철거되지 않은 개미집을 모으는대 시간과 메모리가 필요함
+            good_houses = [house for house in houses if house >= 0]
+            mx_time = good_houses[-1]
+            # mx_time = 1000000000
+            # #2 mx_time을 최대로 설정하는 방법
+
 
             while mn_time <= mx_time:
                 mid_time = (mn_time + mx_time) // 2
@@ -26,9 +41,10 @@ def solve():
                 last_house = -1
                 interval = 1
 
-                for house in houses[1:]:
-                    if house < 0:
-                        continue
+                for house in good_houses[1:]:
+                    # #2 방법으로 풀면, 철거된 집을 skip 하는 아래 코드가 필요합니다.
+                    # if house < 0:
+                    #     continue
 
                     if last_house < 0:
                         last_house = house
