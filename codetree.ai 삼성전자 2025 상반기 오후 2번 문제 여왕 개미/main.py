b3888157d9f05f39ee4249f7a1c23e1741b671d7
ByeongKeon Kim <skysign@gmail.com>
2026-04-16 19:03:25 +0900

    codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미 문제 링크: https://www.codetree.ai/ko/frequent-problems/all/problems/queen-ant

--- codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미/main.py
+++ codetree.ai 삼성전자 2025 상반기 오후 2번 문제 여왕 개미/main.py
@@ -1,53 +1,49 @@
 def solve():
-    houses_pos = [0]  # 여왕 개미의 집
-    houses_deleted = [False]  # 여왕 개미의 집
-
     Q = int(input())
+    houses = [0]  # 여왕 개미의 집을 x=0 위치에 건설합니다.
 
     for _ in range(Q):
         cmds = list(map(int, input().split()))
-        cmd = cmds[0]
 
-        if cmd == 100:
-            for pos in cmds[2:]:
-                houses_pos.append(pos)
-                houses_deleted.append(False)
-        elif cmd == 200:
-            pos_new = cmds[1]
-            houses_pos.append(pos_new)
-            houses_deleted.append(False)
-        elif cmd == 300:
-            pos_deleted = cmds[1]
-            houses_deleted[pos_deleted] = True
-        elif cmd == 400:
-            cnt_ant = cmds[1]
-
-            mn_time, mx_time = 0, 1000000000
+        if cmds[0] == 100:
+            for house in cmds[2:]:
+                houses.append(house)
+        elif cmds[0] == 200:
+            houses.append(cmds[1])
+        elif cmds[0] == 300:
+            q = cmds[1]
+            houses[q] = -1
+        elif cmds[0] == 400:
+            r = cmds[1]
+            mn_time = 0
+            # mx_time = houses[-1]
+            mx_time = 1000000000
             min_time = 0
 
             while mn_time <= mx_time:
                 mid_time = (mn_time + mx_time) // 2
 
-                intervals = 0
-                last_pos = -1000000000
+                last_house = -1
+                interval = 1
 
-                for idx in range(1, len(houses_pos)):
-                    if houses_deleted[idx]: # 삭제된 집인 경우 skip
+                for house in houses[1:]:
+                    if house < 0:
                         continue
 
-                    crnt_pos = houses_pos[idx]
+                    if last_house < 0:
+                        last_house = house
+                    elif house - last_house > mid_time:
+                        interval += 1
+                        last_house = house
 
-                    if crnt_pos - last_pos > mid_time:
-                        last_pos = crnt_pos
-                        intervals += 1
-
-                if intervals <= cnt_ant:
+                if interval <= r:
                     min_time = mid_time
                     mx_time = mid_time - 1
                 else:
                     mn_time = mid_time + 1
 
             print(min_time)
+
 
 if __name__ == '__main__':
     solve()
